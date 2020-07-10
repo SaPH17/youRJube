b46e30c8556e5e61880fed97eafbe8137b747246
SaPH17 <59709896+SaPH17@users.noreply.github.com>
2020-07-10 19:56:55 +0700

    Making navbar and sidebar

--- Frontend/youRJube/src/app/app.component.ts
+++ Frontend/youRJube/src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'youRJube';
+
+  settingsClick(): void {
+    if(document.getElementById('settings-dropdown').style.display == 'block'){
+      document.getElementById('settings-dropdown').style.display = 'none'
+    }
+    else{
+      document.getElementById('settings-dropdown').style.display = 'block'
+    }
+  }
+
+  notifClick(): void {
+    if(document.getElementById('notif-dropdown').style.display == 'block'){
+      document.getElementById('notif-dropdown').style.display = 'none'
+    }
+    else{
+      document.getElementById('notif-dropdown').style.display = 'block'
+    }
+  }
+
+  sideBarCollapse(){
+    if(document.getElementById('sidebar-container').className == 'sidebar-container'){
+      document.getElementById('sidebar-container').className += ' collapse'
+      document.getElementById('content-container').className += ' collapse'
+
+    }
+    else{
+      document.getElementById('sidebar-container').className = 'sidebar-container'
+      document.getElementById('content-container').className = 'content-container'
+    }
+  }
 }
