99efd347fba60c4e60e1f95a422160a9faa1ee51
SaPH17 <59709896+SaPH17@users.noreply.github.com>
2020-07-10 11:51:00 +0700

    First initialization + navbar
    Initialize project
    Add firebase and apollo
    Make navbar

---
+++ Backend/graph/schema.resolvers.go
@@ -0,0 +1,28 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"youRJube/graph/generated"
+	"youRJube/graph/model"
+	"errors"
+)
+
+func (r *queryResolver) Users(ctx context.Context) ([]*model.User, error) {
+	var users []*model.User
+
+	err := r.DB.Model(&users).Select()
+
+	if(err != nil){
+		return nil, errors.New("Failed to query user")
+	}
+
+	return users, nil
+}
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
+
+type queryResolver struct{ *Resolver }
---
+++ Backend/server.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"youRJube/graph"
+	"youRJube/graph/generated"
+	_"youRJube/postgres"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-pg/pg/v10"
+	"github.com/go-chi/chi"
+	"github.com/rs/cors"
+)
+
+const defaultPort = "8080"
+
+func main() {
+
+	router := chi.NewRouter()
+
+	router.Use(cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:4200", "http://localhost:8080"},
+		AllowCredentials: true,
+		Debug:            true,
+	}).Handler)
+
+	// pgDB := postgres.New(&pg.Options{
+	// 	Addr:     "ec2-18-214-119-135.compute-1.amazonaws.com:5432",
+	// 	User:     "qnhpjhiowjeffg",
+	// 	Password: "0fff8683a839d232acd76490c71c3dbd07b851eec1f810ea258af957336b322f",
+	// 	Database: "del57hi1sgal5",
+	// })
+
+	opt, err := pg.ParseURL("postgres://qnhpjhiowjeffg:0fff8683a839d232acd76490c71c3dbd07b851eec1f810ea258af957336b322f@ec2-18-214-119-135.compute-1.amazonaws.com:5432/del57hi1sgal5")
+	if err != nil {
+		panic(err)
+	}
+
+	pgDB := pg.Connect(opt)
+
+	// pgDB.AddQueryHook(postgres.DBLogger{})
+
+	// defer pgDB.Close()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{DB: pgDB}}))
+
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
